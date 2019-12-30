20a29826ebb1b7c6577f8d637da289025fd095db
tejaswigowda <tejaswi@foxyninjastudios.com>
2019-12-29 21:18:16 -0700

    ac

--- custom.js
+++ custom.js
@@ -1,13 +1,15 @@
-var hours = new Date().getHours();
+setTimeout(function(){
+  var hours = new Date().getHours();
 
-if(hours < 7 && hours >=20){
-  $("html").removeClass("rust").addClass("coal");
-}
-else{
-  $("html").removeClass("coal").addClass("rust");
-}
+  if(hours < 7 && hours >=20){
+    $("html").removeClass("rust").addClass("coal");
+  }
+  else{
+    $("html").removeClass("coal").addClass("rust");
+  }
+}, 1000);
 
 
-$("#page-wrapper").on("touchstart", function(){
-  setTimeout($("html").removeClass("sidebar-visible").addClass("sidebar-hidden"), 500);
+$("#content").on("touchstart", function(){
+  setTimeout($("html").removeClass("sidebar-visible").addClass("sidebar-hidden"), 100);
 })

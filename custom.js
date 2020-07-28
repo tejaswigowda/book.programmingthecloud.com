ffe8a7266d54585286eaeb0a7eb389441c5ec29f
tejaswigowda <tejaswi@foxyninjastudios.com>
2020-07-27 22:04:31 -0700

    ac

--- custom.js
+++ custom.js
@@ -25,3 +25,22 @@ setTimeout(function(){
 $("#content").on("touchstart", function(){
   setTimeout('$("html").removeClass("sidebar-visible").addClass("sidebar-hidden")', 100);
 })
+
+
+
+
+
+function putScriptInIframes(script, scriptId) {
+
+   var $iframes = $('iframe');
+   $iframes.each(function () {
+        var thisDoc = this.contentWindow.document;
+        if ( ! thisDoc.getElementById(scriptID)) {
+            var scriptObj = thisDoc.createElement("script");
+            scriptObj.type = "text/javascript";
+            scriptObj.id = scriptId;
+            scriptObj.innerHTML = script;
+            thisDoc.body.appendChild(scriptObj);
+        }
+    });
+}
